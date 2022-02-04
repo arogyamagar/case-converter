9e1addad992a2cd8ec529163f55a41ec7d1dc622
Arogya Thapa Magar <arogyamagar@gmail.com>
2022-02-04 14:39:53 +0545

    fixes

---
+++ src/components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Alert(props) {
+    const capitalization = (word) =>{
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
+  return (
+    <div style = {{height:'50px'}}>
+      {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+          <strong>{capitalization(props.alert.type)}</strong>: {props.alert.message}
+      </div>}
+    </div>
+  )
+}
