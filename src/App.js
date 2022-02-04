00145397d9b7e7b8666c8228441e83da8c2e8540
Arogya Thapa Magar <arogyamagar@gmail.com>
2022-02-04 16:35:03 +0545

    Final Touch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
